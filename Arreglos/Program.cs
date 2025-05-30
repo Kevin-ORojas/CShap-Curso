59261db6ef727b821150c68430ec08708b6a5196
Kevin-ORojas <kevinroj326@gmail.com>
2025-05-29 22:26:03 -0500

    arreglos

---
+++ Arreglos/Program.cs
@@ -0,0 +1,27 @@
+﻿// arreglos son los que pueden almacenar varias variables
+
+using System;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = 
+            [
+                "Mundo", 
+                "juan", 
+                "Pedro", 
+                "ale",
+                "kevin"
+            ];
+
+
+
+      
+
+            Console.WriteLine(friends.Length);
+        }
+    }
+}

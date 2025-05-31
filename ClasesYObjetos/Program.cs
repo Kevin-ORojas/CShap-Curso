f34988eef7f620cd8b2a99d28649dc7dd6ab22d0
Kevin-ORojas <kevinroj326@gmail.com>
2025-05-31 08:24:29 -0500

    clases y objetos

--- ClasesYObjetos/Program.cs
+++ ClasesYObjetos/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClasesYObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sale sale1 = new Sale(100, DateTime.Now);
+            Console.WriteLine(sale1.GetInfo());
+        }
+
+    }
+
+    class Sale
+    {
+        int total;
+        DateTime date;
+
+        public Sale(int total, DateTime time)
+        {
+            //esto es un un constructor por que se llama igual a la clase
+            this.total = total;
+            this.date = time;
+        }
+
+        public string GetInfo()
+        {
+            return total + " " + date.ToLongDateString();
+        }
+        public void Show()
+        {
+            Console.WriteLine("Hola soy una ventana");
+        }
+    }
+}
